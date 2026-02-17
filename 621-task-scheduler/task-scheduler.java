03bb364018f042451eb14b577ff61b7f801687b4
vtu24747-dev <vtu24747@veltech.edu.in>
2026-02-17 09:53:09 +0530

    Time: 2 ms (98.64%) | Memory: 48.2 MB (67.11%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+
+        int[] freq = new int[26];
+        for (char t : tasks) {
+            freq[t - 'A']++;
+        }
+
+        int maxFreq = 0;
+        for (int f : freq) {
+            maxFreq = Math.max(maxFreq, f);
+        }
+
+        int maxCount = 0;
+        for (int f : freq) {
+            if (f == maxFreq) {
+                maxCount++;
+            }
+        }
+
+        return Math.max(tasks.length, (maxFreq - 1) * (n + 1) + maxCount);
+    }
+}
