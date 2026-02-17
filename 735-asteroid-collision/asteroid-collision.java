36153c650b861e867058eccdaf3ac2ed0ed62c5d
vtu24747-dev <vtu24747@veltech.edu.in>
2026-02-17 09:49:24 +0530

    Time: 4 ms (81.26%) | Memory: 46.8 MB (90.59%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.java
@@ -0,0 +1,36 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            boolean destroyed = false;
+
+            while (!stack.isEmpty() && a < 0 && stack.peek() > 0) {
+                if (stack.peek() < -a) {
+                    stack.pop(); // top explodes
+                } else if (stack.peek() == -a) {
+                    stack.pop(); // both explode
+                    destroyed = true;
+                    break;
+                } else {
+                    destroyed = true; // current explodes
+                    break;
+                }
+            }
+
+            if (!destroyed) {
+                stack.push(a);
+            }
+        }
+
+        // Convert stack to array
+        int[] result = new int[stack.size()];
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+}
