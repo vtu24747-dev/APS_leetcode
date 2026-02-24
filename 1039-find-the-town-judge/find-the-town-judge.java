94fbca8d4cd5242768c8756f08ed828f7254efde
vtu24747-dev <vtu24747@veltech.edu.in>
2026-02-24 10:17:07 +0530

    Time: 2 ms (99.73%) | Memory: 53.7 MB (70.68%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] score = new int[n + 1];
+
+        for (int[] t : trust) {
+            score[t[0]]--; // trusts someone
+            score[t[1]]++; // trusted by someone
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1)
+                return i;
+        }
+        return -1;
+    }
+}
